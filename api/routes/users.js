2f933cc40bcf4632a1fabef822e99f886820ceed
adrienWorkday <91661368+adrienWorkday@users.noreply.github.com>
2021-10-01 15:11:12 +0200

    user api route

--- api/routes/users.js
+++ api/routes/users.js
@@ -1,9 +1,33 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
+/* GET all users. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  db.collections("employee").find().toArray().then(
+    results => {
+        console.log("GET request successful, here are the all current users:")
+        console.log(results)
+        console.log("sending results to recipient...")
+        res.send(results);
+    }).catch(error => {
+        console.error(error)
+    })
+});
+/* GET users from a specifc office. */
+router.get('/:officename/:date', function(req, res, next) {
+  db.collections("employee").find().toArray().then(
+    results => {
+        console.log("GET request successful, here are the all the users in "
+        +req.params.officename+" on "+req.params.date+":")
+        console.log(results)
+        console.log("sending results to recipient...")
+        res.send(results);
+    }).catch(error => {
+        console.error(error)
+    })
 });
 
+router.get('/')
+
 module.exports = router;
+
