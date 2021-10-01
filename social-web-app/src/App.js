c40c561641976b6afdf53e58c49f0f252865e6dd
Sinéad Galbraith <sinead.galbraith@workdayinternal.com>
2021-10-01 10:49:49 +0100

    Calendar clicking functionality

--- social-web-app/src/App.js
+++ social-web-app/src/App.js
@@ -4,24 +4,39 @@ import 'react-calendar/dist/Calendar.css';
 import Calendar from 'react-calendar';
 import { Slider } from './Slider';
 import React, { useState } from 'react';
+import EmployeesInOffice from './EmployeesInOffice';
 
 function App() {
   const [value, onChange] = useState(new Date());
   const offices = [{"officeName":"Dublin"}, {"officeName":"Prague"},{"officeName":"Munich"}]
+  const [visibility, setVisibility] = useState(false);
+
+  var container = document.getElementsByClassName('Calendar')[0];
+    document.addEventListener('click', function( event ) {
+      if(container === undefined) {return}
+      if (container !== event.target && !container.contains(event.target)) {    
+        // console.log('clicking outside the div');
+        setVisibility(false)
+      }
+  });
   return (
     <div className="App">
       {Slider(offices)}
-      <Calendar
-        onChange={onChange}
-        value={value}
-        /* 
-          Clicking functionality for calendar, value returned as Date, can be separated into days, months, years etc:
-            onClickDay={(value, event) => console.log('Clicked day: ', value)}
-            value.getDate() 
-            value.getMonth() // returns month-1 value, need to add on 1 again.
-            value.getFullYear()
-        */ 
-        />
+      <div className="Calendar">
+        <Calendar
+          onChange={onChange}
+          value={value}
+          onClickDay={function() {setVisibility(true)}}
+          /* 
+            Clicking functionality for calendar, value returned as Date, can be separated into days, months, years etc:
+              onClickDay={(value, event) => console.log('Clicked day: ', value)}
+              value.getDate() 
+              value.getMonth() // returns month-1 value, need to add on 1 again.
+              value.getFullYear()
+          */ 
+          />
+        {visibility && <EmployeesInOffice />}
+      </div>
     </div>
   );
 }
