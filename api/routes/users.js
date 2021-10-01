b29e130450740cf492b4bd61b8ddb5023aa12add
Johnathan Nicholas <johnathan.nicholas@workday.com>
2021-10-01 14:27:08 +0100

    working on retrieving users based on office and date

--- api/routes/users.js
+++ api/routes/users.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var router = express.Router();
+var db = require('../database/employee')
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', async function(req, res, next) {
+  await db.getAllEmployees().then(
+    results => {
+      console.log("GET request successful. Here is a list of all employees");
+      console.log(results);
+      res.send(results);
+    }).catch(error => {
+      console.error(error)
+    })
 });
 
 module.exports = router;
