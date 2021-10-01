211bda497c85a569c349abbd765932ef0d48aa92
adrienWorkday <91661368+adrienWorkday@users.noreply.github.com>
2021-10-01 10:14:00 +0200

    added slider to main / updated packages

--- social-web-app/src/App.js
+++ social-web-app/src/App.js
@@ -2,12 +2,15 @@ import './App.css';
 import 'react-calendar/dist/Calendar.css';
 
 import Calendar from 'react-calendar';
+import { Slider } from './Slider';
 import React, { useState } from 'react';
 
 function App() {
   const [value, onChange] = useState(new Date());
+  const offices = [{"officeName":"Dublin"}, {"officeName":"Prague"},{"officeName":"Munich"}]
   return (
     <div className="App">
+      {Slider(offices)}
       <Calendar
         onChange={onChange}
         value={value}
