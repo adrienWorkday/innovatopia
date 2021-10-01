3ecb51941e97f41b688345942a51fd9aeaf1ba69
Stefano Puzzuoli <stefano.puzzuoli@workdayinternal.com>
2021-10-01 11:59:39 +0100

    Add styles to calendar

--- social-web-app/src/App.js
+++ social-web-app/src/App.js
@@ -1,27 +1,32 @@
 import './App.css';
-import 'react-calendar/dist/Calendar.css';
 
 import Calendar from 'react-calendar';
 import { Slider } from './Slider';
 import React, { useState } from 'react';
+import EmployeesInOffice from './EmployeesInOffice';
 
 function App() {
   const [value, onChange] = useState(new Date());
   const offices = [{"officeName":"Dublin"}, {"officeName":"Prague"},{"officeName":"Munich"}]
+  let isVisible = false;
   return (
     <div className="App">
+      <div className="app-body">
       {Slider(offices)}
       <Calendar
         onChange={onChange}
         value={value}
+        className='c1'
         /* 
           Clicking functionality for calendar, value returned as Date, can be separated into days, months, years etc:
-            onClickDay={(value, event) => console.log('Clicked day: ', value)}
+            onClickDay={(value, event) => console.log('Clicked day: ', value)} isVisible = !isVisible
             value.getDate() 
             value.getMonth() // returns month-1 value, need to add on 1 again.
             value.getFullYear()
         */ 
         />
+        
+        </div>
     </div>
   );
 }
